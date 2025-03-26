bd93f951e141ca81656bf8fe2450d2f96a51073a
Jose Sanchez <chizaelite1280@gmail.com>
2025-03-26 17:36:29 -0600

    Pruebas de calcular la altura

--- src/ArbolBinario/Nodo.java
+++ src/ArbolBinario/Nodo.java
@@ -3,12 +3,12 @@ package ArbolBinario;
 class Nodo {
 
     int valor;
-    int height;
+    int altura;
     Nodo izquierda, derecha;
 
     public Nodo(int valor) {
         this.valor = valor;
-        this.height = 1;
+        this.altura = 1;
         izquierda = derecha = null;
     }
 }
